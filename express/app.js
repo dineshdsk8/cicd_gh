cff3057160064fc94f7a8932ae10ce5536ef2929
Dinesh <dinesh@gmail.com>
2026-06-11 21:46:48 +0530

    final commit

--- express/app.js
+++ express/app.js
@@ -8,8 +8,10 @@ const backendUrl = "http://flask:6000";
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(backendUrl);
+
     res.send(`
-      <h1>Helo from Devops/h1>
+      <h1>Express Frontend Running</h1>
+      <h3>Node Port: 4000</h3>
       <pre>${JSON.stringify(response.data, null, 2)}</pre>
     `);
   } catch (err) {
@@ -18,5 +20,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(4000, "0.0.0.0", () => {
-  console.log("Express running");
+  console.log("Express running on port 4000");
 });
